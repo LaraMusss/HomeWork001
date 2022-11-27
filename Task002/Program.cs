6269c4bd50d55348901e63ecc52ac902f4b34e90
LaraMusss <musiny_home@rambler.ru>
2022-11-27 21:14:58 +0500

    задание 2 решено через if

---
+++ Task002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Взять два числа на входе, выдать результат какое больше
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else
+{
+    Console.WriteLine("Второе число больше первого");
+}
+
