63242736d007f8acbd8597ed78eb328ef0c90edb
LaraMusss <musiny_home@rambler.ru>
2022-11-27 21:43:35 +0500

    Задача решена через int max

---
+++ Task004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача  принимает на вход три числа и выдает max
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max =a;
+if (b>max) max=b;
+if (c>max) max=c;
+Console.WriteLine(max);
