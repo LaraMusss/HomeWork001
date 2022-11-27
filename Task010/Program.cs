f7d86604793e0e3bb4ec1c38e2e27350b1f35f7d
LaraMusss <musiny_home@rambler.ru>
2022-11-28 04:30:40 +0500

    На вход трехзнач число  на выход цифру вторую

---
+++ Task010/Program.cs
@@ -0,0 +1,5 @@
+﻿// На вход ввести трехзначное значение, на выход вторую цифру этого числа
+Console.WriteLine("Введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a/10;
+Console.WriteLine(b%10);
