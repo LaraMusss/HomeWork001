b327db1b2009ef7275a435b32af7ea9b5f7e793c
LaraMusss <musiny_home@rambler.ru>
2022-11-28 04:48:09 +0500

    Найден выходной и будний день

---
+++ Task015/Program.cs
@@ -0,0 +1,7 @@
+﻿// На вход цифру, на выход выходной или будни
+Console.WriteLine("ВВедите день недели");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day==6 || day==7)
+    Console.WriteLine("выходной");
+else
+    Console.WriteLine("будний день");    
