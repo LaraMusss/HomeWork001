0fa5462383ed6ce21c5d851181819a41bb0ff5eb
LaraMusss <musiny_home@rambler.ru>
2022-11-28 05:23:29 +0500

    на выходе тр цифра или нет цифры, решено через if

---
+++ Task013v1/Program.cs
@@ -0,0 +1,17 @@
+﻿// на входе число, на выходе третью цифру или нет третьей цифры
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N<100)
+{    
+    Console.WriteLine("Нет третьей цифры");
+}
+else if (N<1000)
+    {        
+        
+        Console.WriteLine(N%10);
+    }
+    else if (N<10000)
+        { 
+            N=N/10;
+            Console.WriteLine(N%10);    
+        }
