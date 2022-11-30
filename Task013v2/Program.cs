313a0bd28eac91183df5c67348544a8244663645
LaraMusss <musiny_home@rambler.ru>
2022-11-30 21:16:41 +0500

    поиск третьей цифры

---
+++ Task013v2/Program.cs
@@ -0,0 +1,20 @@
+﻿// выводит 3ю цифру многозначного числа или сообщает что ее нет
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N<100)
+{
+    Console.WriteLine("Нет третьей цифры");
+} 
+else if (N>=100 && N<1000)
+    {
+        Console.WriteLine(N%10);
+    }
+    else 
+    {
+        while (N>1000)
+        {
+            N=N/10;
+        }
+    }
+    
+
