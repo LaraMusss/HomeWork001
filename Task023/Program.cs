8712fab9d22de87e66fb53fe0e494eab7aea864e
LaraMusss <musiny_home@rambler.ru>
2022-11-30 19:49:55 +0500

    на вход число, на выход таблицу кубов до N

---
+++ Task023/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=N; i++)
+{
+    Console.WriteLine(i*i*i);
+}
