ead35295f043e2d08f92f5fc5ade2561597573c9
LaraMusss <musiny_home@rambler.ru>
2022-11-30 19:39:49 +0500

    На вход 5зн число, проверка палиндром

---
+++ Task019/Program.cs
@@ -0,0 +1,15 @@
+﻿// На вход пятизначное число, проверяет является ли оно палиндромом (зеркальным), пример: 23532 является
+Console.WriteLine("Введите пятизначное число по одной цифре через энтер");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int d = Convert.ToInt32(Console.ReadLine());
+int e = Convert.ToInt32(Console.ReadLine());
+if (a==e && b==d)
+{
+    Console.WriteLine("Число является палиндроном");
+}
+else
+{
+    Console.WriteLine("Число не является палиндроном");
+}
