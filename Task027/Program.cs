7dac9e995f5d7c5b1269a053b4c722e6d4539cfc
LaraMusss <musiny_home@rambler.ru>
2022-12-07 17:34:16 +0500

    найти сумму цифр массива и вывести массив

---
+++ Task027/Program.cs
@@ -0,0 +1,14 @@
+﻿// на вход число на выход сколько цифр в числе
+Console.WriteLine("Ведите число");
+string number = Console.ReadLine();
+int sum = 0;
+int count = 0;
+char []array = new char [number.Length];
+for (int i=0; i<array.Length; i++)
+{
+    array[i] = number[i];
+    count++;
+    sum=sum+number[i];
+    Console.WriteLine(number[i]);
+}
+Console.WriteLine(sum);
