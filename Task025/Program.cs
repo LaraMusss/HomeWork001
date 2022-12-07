fd12a307b8b36a859a820750472e8423617a6dd5
LaraMusss <musiny_home@rambler.ru>
2022-12-07 17:13:08 +0500

    возведение в степень

---
+++ Task025/Program.cs
@@ -0,0 +1,16 @@
+﻿// напишите цикл на вход два числа А в степень В 
+Console.WriteLine("Введите число А");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+int i = 0;
+
+while (i<N)
+    {
+    result = result*A;  
+    i++;
+    }
+
+
+Console.WriteLine(result);
