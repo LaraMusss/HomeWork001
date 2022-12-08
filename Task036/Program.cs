503fb8c542e0d2e5749b3c6e45e2a7a17a73de47
LaraMusss <musiny_home@rambler.ru>
2022-12-08 16:21:38 +0500

    массив сл чисел, найти сумму значений нечет эл

---
+++ Task036/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задать одомерный массив случайными числами
+// найти сумму значений элементов нечетных позиций
+Console.WriteLine("Массив случайных чисел");
+int [] array = new int[4];
+int sum =0;
+for (int i=0; i<4; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+    Console.Write(array[i]+" ");
+    if (i%2==0)
+    {
+    }
+    else
+    {
+        sum = sum + array[i];
+    }
+}
+Console.WriteLine($"сумма значений нечетных элементов в массиве = {sum}");
+
