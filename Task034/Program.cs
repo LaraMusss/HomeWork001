a5204f8b3724364c14d08bf1161bd4ef5693e0cc
LaraMusss <musiny_home@rambler.ru>
2022-12-08 16:01:05 +0500

    массив случ 3знчисел, найти четные

---
+++ Task034/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задать массив случайными 3х зн положит числами
+// найти кол-во чет чисел в массиве
+Console.WriteLine("Массив случайных трехзначных чисел");
+int [] array = new int[5];
+int count =0;
+for (int i=0; i<5; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(array[i]+" ");
+    if (array[i]%2==0)
+    {
+        count=count+1;
+    }
+}
+Console.WriteLine($"Количество четных чисел в массиве = {count}");
+
