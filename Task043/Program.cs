53b2ba1d4711f07fcdefd5861eedd5998d878c0e
LaraMusss <musiny_home@rambler.ru>
2022-12-12 00:47:21 +0500

    Точка пересечения двух прямых

---
+++ Task043/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти точку пересеч двух прямых заданных уравнениями
+// y=k1*x+b1  y=k2*x+b2
+// b1 k1 b2 k2 задает пользователь
+Console.WriteLine("Введите b1=");
+double b1=Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите k1=");
+double k1=Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите b2=");
+double b2=Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите k2=");
+double k2=Convert.ToInt32 (Console.ReadLine());
+double x=(b1-b2)/(k2-k1);
+double y=k2*x+b2;
+x=Math.Round(x, 3);
+y=Math.Round(y, 3);
+Console.WriteLine($"Точка пересечения двух прямых ({x},{y})");
+
