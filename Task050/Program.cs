d59693b23e2d7841d911cf9e90a6bb32809f46be
LaraMusss <musiny_home@rambler.ru>
2022-12-12 23:56:47 +0500

    2х мерный массив на вход число, есть ли оно в массиве

---
+++ Task050/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте 2х мерный массив , размер m*n,
+// на вход принимает позицию а возвращает значение элемента
+// или что такого эл-те нет 
+Console.WriteLine("Введите размер массива: количество строк и столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int [m,n];
+for ( int i=0; i<array.GetLength(0); i++)
+{
+    for (int j=0; j<array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(0,50);
+        Console.Write($"{array[i,j]}  ");
+        
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for ( int i=0; i<array.GetLength(0); i++)
+{
+    for (int j=0; j<array.GetLength(1); j++)
+    {
+        if (array[i,j]==Num) 
+            Console.WriteLine("Число есть в массиве");
+        else 
+            Console.WriteLine("Числа нет в массиве"); 
+    }
+    Console.WriteLine();        
+}
+Console.WriteLine();
