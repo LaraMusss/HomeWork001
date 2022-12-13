0a479f5532708515ac210e8bb3b7b6597d1c9822
LaraMusss <musiny_home@rambler.ru>
2022-12-13 22:54:56 +0500

    2х мерный массив, найти ср арифм сумму столбцов

---
+++ Task052/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте 2х мерный массив , размер m*n,
+// заполненный случайными целыми числами,
+// найдите ср арифм элем в каждом столбце
+Console.WriteLine("Введите размер массива: количество строк и столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int [m,n];
+for ( int i=0; i<array.GetLength(0); i++)
+{
+    for (int j=0; j<array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(0,100);
+        Console.Write($"{array[i,j]}  ");
+    }
+    Console.WriteLine();
+}
+
+
+for ( int j=0; j<array.GetLength(1); j++)
+{
+    int sum = 0;
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        
+        if (i==i)
+        sum=(sum+array[i,j]);
+    }
+    Console.WriteLine();
+    
+    Console.WriteLine($"ср арифм сумма столбца = {sum/m} ");
+}
+Console.WriteLine();
