3c0fa46e484941a898c1b68796aac645c111481d
LaraMusss <musiny_home@rambler.ru>
2022-12-25 01:54:41 +0500

    Задать M и N, вывести все натур числа от N до M

---
+++ Task064/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выдает все натуральные числа в промежутке от N до M
+
+Console.WriteLine("Введите целое положительное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int Numbers(int n, int m)
+{
+    Console.WriteLine($"{(n)} ");
+    if (n == m) return m;
+    return Numbers(n - 1, m);     
+}
+Numbers(n,m);
