a2730a4f7909b7e5171a33c03cdeb410f048b02f
LaraMusss <musiny_home@rambler.ru>
2022-12-25 01:32:43 +0500

    Сумма натуралных чисел от M до N

---
+++ Task066/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите целое положительное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ 
+int SumNumbers(int m, int n)
+{
+    if (m == n) return n;
+    return m + SumNumbers(m + 1, n);     
+}
+Console.WriteLine(SumNumbers(m, n));
